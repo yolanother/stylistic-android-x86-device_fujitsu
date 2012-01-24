a616a0cdad8788db12bb9f3911015f925f7c2644
Aaron Jackson <a1.jackson@samsung.com>
2012-01-24 13:03:04 -0600

    Get build working with VESA no hw accel yet.

--- common/generic_fujitsu.mk
+++ common/generic_fujitsu.mk
@@ -1,9 +1,3 @@
-PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
-PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT)
-
-PRODUCT_COPY_FILES := \
-    device/common/generic_x86/GenericTouch.idc:system/usr/idc/GenericTouch.idc \
-
 $(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
 
 PRODUCT_MANUFACTURER := fujitsu
--- stylistic/AndroidBoard.mk
+++
@@ -1,4 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/stylistic_defconfig
-
-include $(GENERIC_X86_ANDROID_MK)
